6ca0bb02828ad13341ad3adb64aeb0acb1771e5b
aditisharma4747 <85553079+aditisharma4747@users.noreply.github.com>
2021-06-15 14:07:39 +0530

    Add files via upload
    Initial commit

---
+++ AARITHMETIC-FUNCTIONS 9 JUNE.R
@@ -0,0 +1,141 @@
+# BASIC ARITHMETIC FUNCTIONS
+1111+11
+677+89
+77-6
+66^2
+66^3
+sqrt(15)
+# RIGHT WAY TO FIND MEAN
+mean (22,33,44)
+c(22,33,44)
+mean(c(22,33,44))
+
+# storing values in a file
+a <-c (22,33,44)
+b <- c(22,33,44, 1, 100 ,3)
+sum(a)
+sum(b)
+mean (a)
+mean(b)
+mode(a)
+mode(b)
+median(b)
+median(a)
+max(a)
+max(b)
+min(a)
+min(b)
+
+# storing values in c as 1     
+c = 1
+
+a = 1
+# storing values in c as 1
+c = 1
+# storing values in c as 1
+c = 1
+# storing values in a file
+a <-c (22,33,44)
+a = c(1,2,3,45,67)
+# to find out what is the in a number
+str(a)
+# to find out what is the in a number
+str(a)
+class(a)
+typeof(a)
+# to represent in the form of string
+b =
+  # to represent in the form of string
+  b = "Aditi"
+str(b)
+class(b)
+typeof(b)
+b
+# series of value
+a <- 1:3
+# series of value
+d <- 1:15
+# series of value
+d <- 1:10
+sum(d)
+str(d)
+mean(d)
+median(d)
+class(d)
+# more irregular series
+e = c(1,4,6,8,50)
+e
+e[3]
+e[5]
+e[3:5]
+e[c(3, 7)]
+e[c(3,7)]
+class(e)
+str(e)
+# more irregular series
+e = c(1,4,6,8,50,55,44)
+# more irregular series
+e = c(1,4,6,8,50,55,44)
+# more irregular series
+e = c(1,4,6,8,50,55,44)
+e
+e[3]  #gives 3rd number
+e[5]
+e[3:5] # gives us the value between 3rd to 5th
+e[c(3,7)]
+class(e)
+str(e)
+e[c(1,6)] #alternative method to get values from data
+e[c(1,6)] # alternative method to get  values from data
+# to convert integers as numbers
+c(1,3, 455,67,890, .654 ,6.77 , 34.65)
+#to round off
+round (c(1,3,455, 5.674 ,5.66, 67))
+abs = c(1,3,455, 5.674 ,5.66, 67)
+str(round (abs))
+#to round off
+round(c(1,3,455,5.674 ,5.66, 67))
+abs -c(1, 22, 3.55 , 66.56)
+#to round off
+round(c(1,3,455,5.674,5.66,67))
+#to round off
+round(c(1,3, 455,67,890, .654 ,6.77,34.65))
+abs = c(1, 22, 3.55,66.56)
+abs = c(1,3, 455,67,890, .654 ,6.77,34.65)
+str(abs)
+str(round (abs))
+#
+n = c("5","54","66" ,"6")
+sum (n)
+
+as.numeric(n)
+class(n)
+sum (c(n))
+sum (as.numeric(n))
+
+# to as true or false
+is.numeric(n)
+is.character(n)
+is.character(n)
+is.numeric(a)
+is.numeric(d)
+is.numeric(b)
+#
+v = as.numeric
+#
+n = as.numeric(n)
+n = as.character(n)
+is.vector(n)
+is.list(n)
+names = c("aditi","palak","orbit")
+names{3}
+names = as.numeric
+names = as.numeric(n)
+# let's create a list
+as.list(c(name))
+# let's create a list
+as.list(c(22,44,34))
+
+# let's create a list
+as.list(c(22,44 ,34))
+list (c(44,46 ,57)) , (c(343,46,68,77))
